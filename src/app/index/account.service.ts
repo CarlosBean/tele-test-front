e7b80d17be18b092dffdf610e890c5efea508a7f
carlos benavides <1992cab@gmail.com>
2021-07-09 18:04:27 -0500

    chore: added save token on localstorage

--- src/app/index/account.service.ts
+++ src/app/index/account.service.ts
@@ -3,19 +3,74 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { IUser } from '../shared/models/user';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor(private http: HttpClient, private router: Router) { }
+  token: string;
+  user: IUser;
+  API_URL = environment.API_URL;
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.loadStorage();
+  }
 
   login(user: { email: string; password: string }): Observable<any> {
-    return this.http.post(environment.API_URL + '/login', user);
+    return this.http.post(environment.API_URL + '/login', user).pipe(
+      map((res: any) => {
+        this.saveStorage(res.data.token);
+        return res;
+      })
+    );
+  }
+
+  renewToken() {
+    const url = environment.API_URL + '/login/newtoken' + '?token=' + this.token;
+    return this.http.get(url).pipe(
+      map((res: any) => {
+        this.saveStorage(res.token);
+      }),
+      catchError((err: any) => {
+        this.logout();
+        // Swal.fire('Error on Session', 'Token cannot be renewed', 'error');
+        return new Observable<any>();
+      })
+    );
+  }
+
+  isLogged() {
+    return this.token && this.token.length > 5;
+  }
+
+  loadStorage() {
+    if (localStorage.getItem('token')) {
+      this.token = localStorage.getItem('token');
+      this.user = this.parseJwt(this.token).user;
+    }
+  }
+
+  saveStorage(token: string) {
+    localStorage.setItem('token', token);
+    this.token = token;
   }
 
   logout() {
-    this.router.navigateByUrl('login');
+    this.token = '';
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
   }
+
+  parseJwt(token: string) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+  };
 }
