087b26564803bb1217a3d44eb442b1ece7b2fc41
carlos benavides <1992cab@gmail.com>
2021-07-09 10:18:29 -0500

    chore: added material module

---
+++ src/app/material.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+// NgModule that includes all Material modules.
+@NgModule({
+    exports: [
+        // Material
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatAutocompleteModule,
+        MatButtonModule,
+        MatIconModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+    ]
+})
+export class MaterialModule { }
