8e4233b8d1ece386e4b4f826e8eec6f88658702c
carlos benavides <1992cab@gmail.com>
2021-07-09 19:56:51 -0500

    chore: added logout button

--- src/app/layouts/sidebar/sidebar.component.ts
+++ src/app/layouts/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AccountService } from 'src/app/index/account.service';
+
 
 @Component({
   selector: 'app-sidebar',
@@ -7,8 +9,14 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  constructor(private accountService: AccountService) { }
+
   options = [
     { title: 'Users', route: 'users', icon: 'account_circle' },
     { title: 'Products', route: 'products', icon: 'account_circle' }
   ];
+
+  logout() {
+    this.accountService.logout();
+  }
 }
